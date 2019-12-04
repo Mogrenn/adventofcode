9ccf7beadae60f9f607bcd1554e337658ebf990f
Brandon Mogren <mogren@hotmail.se>
2019-12-04 18:46:45 +0100

    solved 2p2

--- 2.py
+++ 2.py
@@ -1,19 +1,21 @@
 import math
+import random
+while(True):
+    file = open("2.txt", "rt")
+    num = file.read()
+    splitNum = num.split(",")
+    splitNum[1] = str(random.randint(50,99))
+    splitNum[2] = str(random.randint(0,49))
 
-file = open("2.txt", "rt")
-num = file.read()
+    for x in range(0,len(splitNum),4):
+        if splitNum[x] == "1" or splitNum[x] == 1:
+            splitNum[int(splitNum[x+3])] = (int(splitNum[int(splitNum[x+1])])+int(splitNum[int(splitNum[x+2])]))
+        elif splitNum[x] == "2" or splitNum[x] == 2 :
+            splitNum[int(splitNum[x+3])] = (int(splitNum[int(splitNum[x+1])])*int(splitNum[int(splitNum[x+2])]))
+        else:
+            break;
 
-splitNum = num.split(",")
+    if splitNum[0] == 19690720:
+        break
 
-splitNum[1] = "12"
-splitNum[2] = "2"
-
-for x in range(0,len(splitNum),4):
-    if splitNum[x] == "1" or splitNum[x] == 1:
-        splitNum[int(splitNum[x+3])] = (int(splitNum[int(splitNum[x+1])])+int(splitNum[int(splitNum[x+2])]))
-    elif splitNum[x] == "2" or splitNum[x] == 2 :
-        splitNum[int(splitNum[x+3])] = (int(splitNum[int(splitNum[x+1])])*int(splitNum[int(splitNum[x+2])]))
-    else:
-        break;
-
-print(splitNum)
+print(100*splitNum[1]+splitNum[2])
